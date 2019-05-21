36e6c2cde38dd477d36d4edb103b5865bcb8684e
Aidan Heerdegen <aidan.heerdegen@anu.edu.au>
2019-05-21 14:38:12 +1000

    Add dask-labextension recipe

---
+++ dask-labextension/post-unlink.bat
@@ -0,0 +1,7 @@
+@echo off
+:: We redirect stderr & stdout to conda's .messages.txt; for details, see
+::     http://conda.pydata.org/docs/building/build-scripts.html
+:: Copied from https://github.com/conda-forge/jupyter_contrib_nbextensions-feedstock/
+(
+  "%PREFIX%\Scripts\jupyter" dask-labextension disable --sys-prefix
+) >>"%PREFIX%\.messages.txt" 2>&1
