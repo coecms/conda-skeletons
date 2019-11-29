53cf440f55767ab6a2a641df895df50350429558
Scott Wales <scott.wales@unimelb.edu.au>
2019-11-29 13:54:39 +1100

    Update activate scripts

--- openmpi/activate.sh
+++ openmpi/activate.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-module load openmpi/4.0.1
+if [ -f /opt/Modules/v4.3.0/libexec/modulecmd.tcl ]; then
+
+    # Gadi
+    export MODULEPATH=/g/data/hh5/public/modules:/apps/Modules/modulefiles
+    eval `/usr/bin/tclsh /opt/Modules/v4.3.0/libexec/modulecmd.tcl sh "load openmpi/4.0.1"`
+
+elif [ -f /opt/Modules/3.2.6/bin/modulecmd ]; then
+
+    # Raijin
+    export MODULEPATH=/g/data/hh5/public/modules:/apps/Modules/modulefiles
+    eval `/opt/Modules/3.2.6/bin/modulecmd sh load openmpi/4.0.1`
+
+fi
