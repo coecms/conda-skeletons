125c363cc19ec3f71759bd9cfd9886d0e67245c7
Scott Wales <scottwales@outlook.com.au>
2020-05-26 11:14:28 +1000

    Add cartopy central config

---
+++ cartopy_userconfig/cartopy_userconfig.py
@@ -0,0 +1,3 @@
+def update_config(config):
+    config['pre_existing_data_dir'] = '/g/data/hh5/public/apps/cartopy-data'
+
---
+++ cartopy_userconfig/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(
+        name='cartopy_userconfig',
+        version='1',
+        py_modules=['cartopy_userconfig']
+        )
