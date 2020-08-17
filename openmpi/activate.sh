955b8d976733286527b18b70f873b765981a10af
Scott Wales <scott.wales@unimelb.edu.au>
2020-08-17 16:19:57 +1000

    Update mpi

--- openmpi/activate.sh
+++ openmpi/activate.sh
@@ -4,12 +4,12 @@ if [ -f /opt/Modules/v4.3.0/libexec/modulecmd.tcl ]; then
 
     # Gadi
     export MODULEPATH=/g/data/hh5/public/modules:/apps/Modules/modulefiles
-    eval `/usr/bin/tclsh /opt/Modules/v4.3.0/libexec/modulecmd.tcl sh "load openmpi/4.0.1"`
+    eval `/usr/bin/tclsh /opt/Modules/v4.3.0/libexec/modulecmd.tcl sh "load openmpi/4.0.2"`
 
 elif [ -f /opt/Modules/3.2.6/bin/modulecmd ]; then
 
     # Raijin
     export MODULEPATH=/g/data/hh5/public/modules:/apps/Modules/modulefiles
-    eval `/opt/Modules/3.2.6/bin/modulecmd sh load openmpi/4.0.1`
+    eval `/opt/Modules/3.2.6/bin/modulecmd sh load openmpi/4.0.2`
 
 fi
